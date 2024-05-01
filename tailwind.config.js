c95426c23b43d5f4c9980cb638edb03075797a35
Kaan <kaan_1536@outlook.com>
2024-05-01 15:35:47 +0300

    commit updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,44 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        noto: ["Noto Sans", "sans-serif"],
+        roboto: ["Roboto", "sans-serif"],
+        bebas: ["Bebas Neue", "sans-serif"],
+        gemunu: ["Gemunu Libre", "sans-serif"],
+        open: ["Open Sans", "sans-serif"],
+      },
+      colors: {
+        "rex-dark": "#48416C",
+        "rex-softdark": "#48416C ",
+        "rex-blue": "#9AC1ED",
+        "rex-yellow": "#EDC18D",
+        "rex-purp": "#B79BF2",
+        "rex-peach": "#EFAB9C",
+        "rex-grey": "#DDE7F2",
+        "rex-white": "#FEFBF6",
+        "rex-bej": "#E7D8B9",
+        "rex-cuteorange": "#F1C9A9",
+        "rex-orange": "#D8D8D8 ",
+        "rex-blood": "#CD5D7D",
+        "rex-cuteblood": "#FF004D",
+        "rex-darkbrown": "#6C5B7B",
+        "rex-cutepurple": "#D0A2F7",
+        "rex-white": "#F1EAFF",
+        "rex-pastel": "#BEADFA",
+        "rex-darkblue": "#1C1678",
+        "rex-softsun": "#C08B5C",
+        "rex-green": "#1FAB89",
+        "rex-text": "#333333",
+        "rex-gold": "#FFD700",
+        "rex-cutecream": "#FFFDD0 ",
+        // "rex-softgray": "#AC7D88",
+      },
+      spacing: {
+        128: "32rem",
+      },
+    },
   },
   plugins: [],
 };
